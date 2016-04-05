d37279dd43d571b8dfd38a283579bf56143fec23
Julian Pidancet <julian@pidancet.net>
2016-04-05 17:08:53 +0100

    xpdm/uxendisp: automatically adjust memory allocation when changing mode

--- vm-support/windows/uxendisp/xddm/miniport/hw.c
+++ vm-support/windows/uxendisp/xddm/miniport/hw.c
@@ -44,6 +44,16 @@ static void uxdisp_crtc_write(PDEVICE_EXTENSION dev, ULONG crtc, ULONG reg, ULON
     uxdisp_write(dev, UXDISP_REG_CRTC(crtc) + reg, val);
 }
 
+static ULONG uxdisp_alloc_read(PDEVICE_EXTENSION dev, ULONG alloc, ULONG reg)
+{
+    uxdisp_read(dev, UXDISP_REG_ALLOC(alloc) + reg);
+}
+
+static void uxdisp_alloc_write(PDEVICE_EXTENSION dev, ULONG alloc, ULONG reg, ULONG val)
+{
+    uxdisp_write(dev, UXDISP_REG_ALLOC(alloc) + reg, val);
+}
+
 VP_STATUS hw_init(PDEVICE_EXTENSION dev)
 {
     if (uxdisp_read(dev, UXDISP_REG_MAGIC) != UXDISP_MAGIC)
@@ -167,6 +177,9 @@ VP_STATUS hw_set_mode(PDEVICE_EXTENSION dev, VIDEO_MODE_INFORMATION *mode)
         return ERROR_INVALID_PARAMETER;
     }
 
+    uxdisp_alloc_write(dev, 0, UXDISP_REG_ALLOC_PAGE_START, 0);
+    uxdisp_alloc_write(dev, 0, UXDISP_REG_ALLOC_PAGE_COUNT,
+                       (height * stride + 4095) >> 12);
     uxdisp_crtc_write(dev, 0, UXDISP_REG_CRTC_ENABLE, 1);
     uxdisp_crtc_write(dev, 0, UXDISP_REG_CRTC_XRES, width);
     uxdisp_crtc_write(dev, 0, UXDISP_REG_CRTC_YRES, height);
@@ -188,7 +201,7 @@ VP_STATUS hw_disable(PDEVICE_EXTENSION dev)
 
 ULONG hw_get_vram_size(PDEVICE_EXTENSION dev)
 {
-    return 1 << uxdisp_read(dev, UXDISP_REG_BANK_ORDER);
+    return uxdisp_read(dev, UXDISP_REG_VRAM_SIZE);
 }
 
 BOOLEAN hw_pointer_update(PDEVICE_EXTENSION dev, ULONG width, ULONG height,
