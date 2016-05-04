cc492d37b37ce7e764555d93421aaa0d24ef41c6
Piotr Foltyn <piotr.foltyn@gmail.com>
2016-05-04 06:28:54 +0100

    KRY-29173: no need to send anything to foreground window if we force mode change

--- vm-support/windows/uxenevent/display.c
+++ vm-support/windows/uxenevent/display.c
@@ -226,9 +226,12 @@ display_resize(int w, int h, unsigned int flags)
 
     SendResolutionToHidDriver();
     SystemParametersInfo(SPI_SETWORKAREA, 0, &work_area, SPIF_UPDATEINIFILE);
-    fgwnd = GetForegroundWindow();
-    owner = GetWindow(fgwnd, GW_OWNER);
-    SetWindowPos((owner) ? owner : fgwnd, HWND_TOP, 0, 0, w, h, SWP_NOOWNERZORDER | SWP_SHOWWINDOW);
+
+    if (!force_change) {
+        fgwnd = GetForegroundWindow();
+        owner = GetWindow(fgwnd, GW_OWNER);
+        SetWindowPos((owner) ? owner : fgwnd, HWND_TOP, 0, 0, w, h, SWP_NOOWNERZORDER | SWP_SHOWWINDOW);
+    }
 
     if (virtual_w != current_w)
         SetWindowPos(right_window, HWND_TOPMOST, virtual_w, 0, current_w, current_h, SWP_SHOWWINDOW);
@@ -337,7 +340,6 @@ display_init(void)
     D3DKMT_OPENADAPTERFROMHDC open_adapter_info;
     UXENDISPCustomMode cm;
     RECT work_area = {0};
-    HWND fgwnd, owner;
 
     /* Sanity check here */
     FillMemory(&dispDevice, sizeof(DISPLAY_DEVICE), 0);
@@ -390,11 +392,7 @@ display_init(void)
         work_area.bottom = virtual_h;
         work_area.right = virtual_w;
         SystemParametersInfo(SPI_SETWORKAREA, 0, &work_area, SPIF_UPDATEINIFILE);
-        fgwnd = GetForegroundWindow();
-        owner = GetWindow(fgwnd, GW_OWNER);
-        SetWindowPos((owner) ? owner : fgwnd, HWND_TOP, 0, 0, virtual_w, virtual_h, SWP_NOOWNERZORDER | SWP_SHOWWINDOW);
     }
-
 
     blank_thread = CreateThread(NULL, 0, blank_loop, NULL, 0, NULL);
     if (!blank_thread) {
