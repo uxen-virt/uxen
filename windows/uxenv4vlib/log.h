3d540e4d9b3280479f88ad1977dc86bc10bd3ba6
Christian Limpach <Christian.Limpach@gmail.com>
2016-06-17 13:39:35 -0400

    uxenv4vlib: fix uxen_v4v_log for debug builds
    also use DbgPrintEx and translate verbosity levels

--- windows/uxenv4vlib/log.h
+++ windows/uxenv4vlib/log.h
@@ -3,24 +3,36 @@
  * SPDX-License-Identifier: ISC
  */
 
-#ifndef __UXENV4VLOG_H__
-#define __UXENV4VLOG_H__
+#ifndef __UXENV4VLIB_LOG_H__
+#define __UXENV4VLIB_LOG_H__
 
 extern uxen_v4v_logger_t uxen_v4v_logger;
 
+#if DBG
+/* KdPrint((fmt, ##__VA_ARGS__)) doesn't work if __VA_ARGS__ is empty,
+ * also translate verbosity levels */
+#define uxen_v4v_KdPrintEx(id, l, fmt, ...) do {                        \
+        DbgPrintEx(id, (l) >= V4VLOG_VERBOSE ? DPFLTR_INFO_LEVEL :      \
+                   ((l) >= V4VLOG_INFO ? DPFLTR_TRACE_LEVEL :           \
+                    ((l) >= V4VLOG_WARNING ? DPFLTR_WARNING_LEVEL :     \
+                     DPFLTR_ERROR_LEVEL)), fmt, ##__VA_ARGS__);         \
+    } while (0)
+#else
+#define uxen_v4v_KdPrintEx(id, l, fmt, ...)
+#endif
+
 #define uxen_v4v_log(lvl, fmt, ...) {                                   \
         if (uxen_v4v_logger) {                                          \
-            char buf[256];                                              \
+            char buf[320];                                              \
             RtlStringCbPrintfA(buf, sizeof(buf), fmt, ##__VA_ARGS__);   \
             uxen_v4v_logger(lvl, buf);                                  \
-        } else {                                                        \
-            KdPrint((fmt, ##__VA_ARGS__));                              \
-        }                                                               \
+        } else                                                          \
+            uxen_v4v_KdPrintEx(DPFLTR_DEFAULT_ID, lvl, fmt, ##__VA_ARGS__); \
     }                                                                   \
 
-#define _Trace(lvl, fmt, ...) \
-    uxen_v4v_log(lvl, "uxenv4vlib:" __FUNCTION__ ": "); \
-    uxen_v4v_log(lvl, fmt, ##__VA_ARGS__)
+#define _Trace(lvl, fmt, ...)                           \
+    uxen_v4v_log(lvl, "uxenv4vlib: %s:%d: " fmt,        \
+                 __FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 #define _TraceVerbose(fmt, ...) _Trace(V4VLOG_VERBOSE, fmt, ##__VA_ARGS__)
 #define _TraceNotice(fmt, ...)  _Trace(V4VLOG_NOTICE, fmt, ##__VA_ARGS__)
@@ -37,4 +49,4 @@ extern uxen_v4v_logger_t uxen_v4v_logger;
 #define TraceWarning(a) do { _TraceWarning a; } while (0)
 #define TraceError(a) do { _TraceError a; } while (0)
 
-#endif
+#endif  /* __UXENV4VLIB_LOG_H__ */
