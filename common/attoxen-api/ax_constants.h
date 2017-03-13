3ac6453eccd817055b0a136cdbdc778bab48d9d6
Kris Uchronski <kuchronski@gmail.com>
2017-03-13 16:04:47 +0000

    ax: update attoxen-api to ax:64be8fb
    * add documention for ax cpuid leafs
    * add two new features: L2_VMCLEAR and L2_FLUSHTLB

--- common/attoxen-api/ax_constants.h
+++ common/attoxen-api/ax_constants.h
@@ -6,34 +6,154 @@
 #ifndef __AX_CONSTANTS_H__
 #define __AX_CONSTANTS_H__
 
+//Probably need a better place to do Atto call documentation - but here will do for now
+
+
 #define AX_CPUID_EFISTUB      0x35271645
+// LEGACY - 
+
 #define AX_CPUID_LEVEL        0x351f9005
+// DEPRECIATED - GET CURRENT LEVEL
+// returns string in RBX,RDX,RCX giving some AX info including the current
+// nesting level.
+
 #define AX_CPUID_CONFIG       0x35655f10
+// READ FROM CONFIG SPACE
+// on entry RCX is offset into config space to read. on exit RBX and RDX
+// contain 16 bytes of config space at that offset and RCX contains the
+// config_space length
+
 #define AX_CPUID_INFO         0x358a719a
+// DEPRECIATED - READ FROM INFO SPACE
+// on entry RCX contains offset into info space, RCX = 0 regenerates info.
+// on exit RBX and RDX gives 16 bytes of info space and RCX gives
+// the number of bytes read.
+
+
 #define AX_CPUID_LOG          0x3509cfe1
+// READ FROM LOG SPACE
+// on entry RCX contains offset into log space. 
+// on exit RAX, RBX, RCX, RDX contain upto 32 bytes of log data
+
 #define AX_CPUID_LOG_SIZE     0x3575d04f
+// READ LOG SIZE
+// on exit RBX contains the size of log space
+// RCX the log read offset 
+// RDX the log write offset
+
 #define AX_CPUID_PRESENCE     0x35f9e064
 #define AX_CPUID_PRESENCE_RDX 0xfee1dead
+// TEST FOR AX
+// on exit RDX has the value AX_CPUID_PRESENCE_RDX
+
 #define AX_CPUID_EBS          0x35ce0403
+// the AX bootloader hooks EBS, and uses this CPUID to communicate
+// to AX from L1 that L1 EBS has succesfully returned.
+// AX then takes over the remaining cpus in the platform, and returns.
+
 #define AX_CPUID_VMINVGPA     0x356d647a
+// unknown - tomasz?
 #define AX_CPUID_VMINVGPAS    0x35372de9
+// unknown - tomasz?
+
 #define AX_CPUID_V4V          0x35dcd68c
+// RESERVED - for ax V4V implementation
+
 #define AX_CPUID_GA_LOOKUP    0x352a4ca0
+// Lookup guest accessable symbol information
+// on entry RCX contains the symbol number
+// on exit RAX contains type and flags information see ga_types.h
+// RBX contains the offset in the GA section where the name of the symbol
+// is stored.
+// RCX contains the offset in the GA section were the data of the symbol
+// is stored
+// RDX contains the size of the symbol
+
+
 #define AX_CPUID_GA_READ      0x35452ce1
+// Reads upto 16 bytes of GA data into RBX, RDX
+// on entry RCX is offset. 
+
 #define AX_CPUID_AX_FEATURES  0x351f9505
+// reads 32 AX feature bits from [RCX<<5 + 31,RCX <<5] into RDX
+
 #define AX_CPUID_LN_FEATURES  0x3583c6ea
+// reads 32 LN feature bits from [RCX<<5 + 31,RCX <<5] into RDX
+
 #define AX_CPUID_TREE_LOG     0x3527bbda
+// READ TREE DATA
+// on entry RCX is offset into tree data
+// on exit RBX, RDX contain upto 16 bytes of tree data
+// RCX contains the quantity of tree data available
+// RAX is non zero to indicate the tree log has been truncated
+
 #define AX_CPUID_PV_EPT_WRITE 0x35091554
 #define AX_CPUID_PV_EPT_WRITE_LEVEL_MASK        0x07
 #define AX_CPUID_PV_EPT_WRITE_VALID             0x10
 #define AX_CPUID_PV_EPT_WRITE_INVEPT_ALL        0x20
+// Update EPT entry. The guest can use this interface to avoid
+// having to issue invept when it does certain modifications of L23
+// EPT tables. After modifying the table the guest should call this 
+// function.  
+//
+// on Entry
+// RBX = L3 EPT_POINTER 
+// RCX = L3 GPA | flags | level 
+// RDX = EPTE that was written
+//
+// The valid flag must always be set
+//
+// AX will update the relevant L03 table (without necessarily consulting the L23 table)
+// if the INVEPT_ALL bit is set in the flags, AX will then proceeed to ensure
+// the L3 mapping is flushed on all cpus.
+//
+// NOTE this is the WRONG primitive to do this, a guest should:
+// 1) write an invalid entry into L23
+// 2) ask AX to:
+// 3)   write an invalid entry into L03
+// 4)   flush the L03 mappings on all cpus
+// 5)   write the correct entry in L03
+// 6) write the correct entry in L23
+//
+// uXen doesn't currently do 1) so we instead provide this primitive.
+// the two are equivalent provided there are <3 guest cpus and updates
+// are always RO->RW.
+
 #define AX_CPUID_INVEPT_ALL   0x359ee514
+// INVEPT ALL L3 CPUS
+// when this call returns AX garuntees that all CPUs will flush
+// their cached L03 EPT entries before executing any more L3 code.
+// It does this by setting a flag on all cpus requring a flush before
+// L3 entry, and then ensures that no cpus are in L3 (by issuing IPIs)
+
+#define AX_CPUID_FLUSHTLB     0x35630ed1
+// FLUSH TLB
+// when this call returns AX garuntees that all CPUs (including this one)
+// at this same level of virtualization will flush all cached VA->PA mappings
+// before executing any more code at this level.
+
+#define AX_CPUID_VMCLEAR      0x35ac6b48
+// VMCLEAR on other CPU
+// on entry RCX is the PA of the VMCS in question
+// if the VMCS is not loaded on any CPU, AX will return 0 in RAX
+// if it is in use AX will issue an IPI to cause it to be unloaded 
+// and return non-zero in RAX
+// The guest should then pause and attempt the operation again until
+// zero is returned or scheduling happens.
+//
+// NB IT IS THE GUEST'S RESPONSIBILITY NOT TO SHOOT ITSELF IN THE FOOT
+// if a guest attempts a vmread/write/launch/resume with a vmcs that
+// has been cleared from another cpu the operation will fail as if
+// the vmcs had been loaded. Guests should use locks &c. to avoid misery.
+
+//  printf 0x35%02x%02x%02x\\n $[ $RANDOM % 256 ] $[ $RANDOM % 256 ] $[ $RANDOM % 256 ]
 
 
-
-#define AX_FEATURES_AX_L1_VMX     (1ULL << 0)
-#define AX_FEATURES_AX_L2_VMX     (1ULL << 1)
-#define AX_FEATURES_AX_SHADOW_EPT (1ULL << 2)
+#define AX_FEATURES_AX_L1_VMX      (1ULL << 0)
+#define AX_FEATURES_AX_L2_VMX      (1ULL << 1)
+#define AX_FEATURES_AX_SHADOW_EPT  (1ULL << 2)
+#define AX_FEATURES_AX_L2_VMCLEAR  (1ULL << 3)
+#define AX_FEATURES_AX_L2_FLUSHTLB (1ULL << 4)
 
 #define AX_FEATURES_LN_VMCS_X_V1    (1ULL << 0)
 #define AX_FEATURES_LN_NO_XCR0      (1ULL << 1)
