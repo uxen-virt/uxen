b6eb048eb749034cc1a1a71bb25a67979f9826b0
Piotr Foltyn <piotr.foltyn@gmail.com>
2017-06-14 12:18:03 +0100

    Revert "Bring sdk inline with what Krypton expects"
    This reverts commit b6b9b2757a32272c4c52fcfe5dc5670d65ef3432.

--- windows/uxenv4vlib/gh_v4vapi.h
+++ windows/uxenv4vlib/gh_v4vapi.h
@@ -51,20 +51,9 @@
 #define __V4VAPI_H__
 
 #if !defined(XENV4V_DRIVER)
-#if defined(_MSC_VER)
-#if defined(_M_X64)
-#define __x86_64__
-#elif defined(_M_IX86)
-#define __i386__
-#endif
-#include <stdint.h>
-#include <Windows.h>
-#include <WinIoCtl.h>
-#endif
 #define V4V_EXCLUDE_INTERNAL
 #include <xen/v4v.h>
 #endif
-
 
 /* This structure is used for datagram reads and writes. When sending a
  * datagram, extra space must be reserved at the front of the buffer to
@@ -351,7 +340,7 @@ _v4v_open(v4v_channel_t *channel, ULONG ring_size, ULONG flags, OVERLAPPED *ov)
         return FALSE;
 
     init.ring_length = ring_size;
-    init.rx_event = CreateEvent(NULL, FALSE, FALSE, NULL);
+    init.rx_event = CreateEvent(NULL, TRUE, FALSE, NULL);
     if (init.rx_event == NULL) {
         CloseHandle(hd);
         return FALSE;
