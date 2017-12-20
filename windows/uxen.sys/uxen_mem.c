6905c458c578fe7053ab3575a1afad67ba1bd162
Bromium Developer <opensource-developer@bromium.com>
2017-12-20 18:57:38 +0000

    Revert "patch rs3-misery"
    This reverts commit ebf3ec98700dbd2f1011d1ee4d8c422eae017328.

--- windows/uxen.sys/uxen_mem.c
+++ windows/uxen.sys/uxen_mem.c
@@ -101,7 +101,7 @@ set_linear_pt_va(void)
         goto out;
     }
 
-    cr3 = read_cr3() & ~(0xfffULL) ;
+    cr3 = read_cr3();
 
     mdl->MdlFlags = MDL_PAGES_LOCKED;
     MmGetMdlPfnArray(mdl)[0] = cr3 >> PAGE_SHIFT;
