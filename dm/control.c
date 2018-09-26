310808924e2e5470ccab3bd0ecc06080504f257b
Tomasz Wroblewski <tomasz.wroblewski@gmail.com>
2018-09-26 09:38:38 +0200

    KRY-47050 dm/shared folders: allow redirect API in suspend state

--- dm/control.c
+++ dm/control.c
@@ -1282,6 +1282,7 @@ struct dict_rpc_command control_commands[] = {
       }, },
 #if defined(CONFIG_VBOXDRV)
     { "sf-add-redirect", control_command_sf_add_redirect,
+      .flags = CONTROL_SUSPEND_OK,
       .args = (struct dict_rpc_arg_desc[]) {
             { "name", DICT_RPC_ARG_TYPE_STRING, .optional = 0 },
             { "src", DICT_RPC_ARG_TYPE_STRING, .optional = 0 },
@@ -1299,6 +1300,7 @@ struct dict_rpc_command control_commands[] = {
         }
     },
     { "sf-del-redirect", control_command_sf_del_redirect,
+      .flags = CONTROL_SUSPEND_OK,
       .args = (struct dict_rpc_arg_desc[]) {
             { "name", DICT_RPC_ARG_TYPE_STRING, .optional = 0 },
             { "src", DICT_RPC_ARG_TYPE_STRING, .optional = 0 },
